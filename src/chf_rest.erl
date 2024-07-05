9ecadea77823b9362d4597ec9badfea7cdd2a529
Vance Shipley <vances@sigscale.org>
2024-07-05 12:50:47 +0800

    rest utility library module

---
+++ src/chf_rest.erl
@@ -0,0 +1,118 @@
+%%% chf_rest.erl
+%%% vim: ts=3
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%%% @copyright 2024 SigScale Global Inc.
+%%% @end
+%%% Licensed under the Apache License, Version 2.0 (the "License");
+%%% you may not use this file except in compliance with the License.
+%%% You may obtain a copy of the License at
+%%%
+%%%     http://www.apache.org/licenses/LICENSE-2.0
+%%%
+%%% Unless required by applicable law or agreed to in writing, software
+%%% distributed under the License is distributed on an "AS IS" BASIS,
+%%% WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+%%% See the License for the specific language governing permissions and
+%%% limitations under the License.
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%%% @doc This library module implements functions for REST in the
+%%%   {@link //chf. chf} application.
+%%%
+-module(chf_rest).
+-copyright('Copyright (c) 2024 SigScale Global Inc.').
+-author('Vance Shipley <vances@sigscale.org>').
+
+%% export the chf_rest  public API
+-export([now/0, date/1, iso8601/1]).
+
+% calendar:datetime_to_gregorian_seconds({{1970,1,1},{0,0,0}})
+-define(EPOCH, 62167219200).
+
+%%----------------------------------------------------------------------
+%%  The chf_rest public API
+%%----------------------------------------------------------------------
+
+-spec now() -> ISO8601
+	when
+		ISO8601 :: string().
+%% @doc Returns the current time in ISO 8601 format.
+now() ->
+	chf_rest:iso8601(erlang:system_time(millisecond)).
+
+-spec date(DateTime) -> DateTime
+	when
+		DateTime :: MilliSeconds | calendar:datetime(),
+		MilliSeconds :: pos_integer().
+%% @doc Convert between unix epoch milliseconds and date and time.
+date({{_, _, _}, {_, _, _}} = DateTime) ->
+	(calendar:datetime_to_gregorian_seconds(DateTime) - ?EPOCH) * 1000;
+date(MilliSeconds) when is_integer(MilliSeconds) ->
+	Seconds = ?EPOCH + (MilliSeconds div 1000),
+	calendar:gregorian_seconds_to_datetime(Seconds).
+
+-spec iso8601(DateTime) -> DateTime
+	when
+		DateTime :: pos_integer() | string() | binary().
+%% @doc Convert between ISO 8601 and Unix epoch milliseconds.
+iso8601(MilliSeconds) when is_integer(MilliSeconds) ->
+	{{Year, Month, Day}, {Hour, Minute, Second}} = date(MilliSeconds),
+	DateFormat = "~4.10.0b-~2.10.0b-~2.10.0b",
+	TimeFormat = "T~2.10.0b:~2.10.0b:~2.10.0b.~3.10.0b",
+	Chars = io_lib:fwrite(DateFormat ++ TimeFormat,
+			[Year, Month, Day, Hour, Minute, Second, MilliSeconds rem 1000]),
+	lists:flatten(Chars);
+iso8601(ISODateTime) when is_binary(ISODateTime) ->
+	iso8601(binary_to_list(ISODateTime));
+iso8601(ISODateTime) when is_list(ISODateTime) ->
+	case string:rchr(ISODateTime, $T) of
+		0 ->
+			iso8601(ISODateTime, []);
+		N ->
+			iso8601(lists:sublist(ISODateTime, N - 1),
+				lists:sublist(ISODateTime,  N + 1, length(ISODateTime)))
+	end.
+%% @hidden
+iso8601(Date, Time) when is_list(Date), is_list(Time) ->
+	D = iso8601_date(string:tokens(Date, ",-"), []),
+	{H, Mi, S, Ms} = iso8601_time(string:tokens(Time, ":."), []),
+	date({D, {H, Mi, S}}) + Ms.
+%% @hidden
+iso8601_date([[Y1, Y2, Y3, Y4] | T], _Acc) ->
+	Y = list_to_integer([Y1, Y2, Y3, Y4]),
+	iso8601_date(T, Y);
+iso8601_date([[M1, M2] | T], Y) when is_integer(Y) ->
+	M = list_to_integer([M1, M2]),
+	iso8601_date(T, {Y, M});
+iso8601_date([[D1, D2] | T], {Y, M}) ->
+	D = list_to_integer([D1, D2]),
+	iso8601_date(T, {Y, M, D});
+iso8601_date([], {Y, M}) ->
+	{Y, M, 1};
+iso8601_date([], {Y, M, D}) ->
+	{Y, M, D}.
+%% @hidden
+iso8601_time([H1 | T], []) ->
+	H = list_to_integer(H1),
+	iso8601_time(T, H);
+iso8601_time([M1 | T], H) when is_integer(H) ->
+	Mi = list_to_integer(M1),
+	iso8601_time(T, {H, Mi});
+iso8601_time([S1 | T], {H, Mi}) ->
+	S = list_to_integer(S1),
+	iso8601_time(T, {H, Mi, S});
+iso8601_time([], {H, Mi}) ->
+	{H, Mi, 0, 0};
+iso8601_time([Ms1 | T], {H, Mi, S}) ->
+	Ms = list_to_integer(Ms1),
+	iso8601_time(T, {H, Mi, S, Ms});
+iso8601_time([], {H, Mi, S}) ->
+	{H, Mi, S, 0};
+iso8601_time([], {H, Mi, S, Ms}) ->
+	{H, Mi, S, Ms};
+iso8601_time([], []) ->
+	{0,0,0,0}.
+
+%%----------------------------------------------------------------------
+%%  internal functions
+%%----------------------------------------------------------------------
+
